3ac54b3894b4a6bd86f82a098a5684babf703c2c
Seddik hakime <seddikhakime@macpro.lowtem.hokudai.ac.jp>
2013-03-11 16:55:05 +0900

    - Add the method FEMJob_addMeshCoordinatesAndTimeModel: to the FEMJob class. - Rename FEMJob_addSolutions: to FEMJob_addSolutionsModel:.

--- Saino/FEMJob.m
+++ Saino/FEMJob.m
@@ -14,12 +14,28 @@
 #import "memory.h"
 
 @interface FEMJob ()
--(void)FEMJob_addSolutions:(FEMModel *)model;
+-(void)FEMJob_addSolutionsModel:(FEMModel *)model;
+-(void)FEMJob_addMeshCoordinatesAndTimeModel:(FEMModel *)model;
 @end
 
 @implementation FEMJob {
     
     BOOL _transient;
+    double *_sTime;
+    double *_sStep;
+    double *_sInterval;
+    double *_sSize;
+    double **_sPrevSizes;
+    double *_steadyIt;
+    double *_nonLinIt;
+    int _sizeSTime;
+    int _sizeSStep;
+    int _sizeSInterval;
+    int _sizeSSize;
+    int _sizeSteadyIt;
+    int _sizeNonLinIt;
+    int _size1SPrevSizes;
+    int _size22SPrevSizes;
 }
 
 @synthesize model = _model;
@@ -39,7 +55,10 @@
 
 }
 
--(void)FEMJob_addSolutions:(FEMModel *)model {
+/***********************************
+    Add flags for active sokutions
+***********************************/
+-(void)FEMJob_addSolutionsModel:(FEMModel *)model {
     
     int i, j;
     BOOL initSolution, found;
@@ -98,6 +117,69 @@
         [utilities addEquationBasicsToSolution:solution name:eq model:model transient:_transient];
         [utilities addEquationToSolution:solution model:model transient:_transient];
     }
+}
+
+/*******************************************************************
+    Add coordinate and time variables to the meshe(s) in the model
+*******************************************************************/
+-(void)FEMJob_addMeshCoordinatesAndTimeModel:(FEMModel *)model {
+    
+    FEMVariable *dtVar;
+    FEMSolution *solution;
+    FEMUtilities *utilities;
+    Nodes_t *nodes;
+    variableArraysContainer *varContainers = NULL, *dtVarContainers = NULL;
+    BOOL found;
+    
+    utilities = [[FEMUtilities alloc] init];
+    
+    varContainers = allocateVariableContainer();
+    solution = nil;
+    for (FEMMesh *mesh in model.meshes) {
+        nodes = mesh.getNodes;
+        
+        varContainers->Values = nodes->x;
+        varContainers->sizeValues = nodes->numberOfNodes;
+        [utilities addVariableTo:mesh.variables mesh:mesh solution:solution name:@"coordinate 1" dofs:1 container:varContainers component:NO ifOutput:NULL ifSecondary:NULL type:NULL];
+        
+        varContainers->Values = nodes->y;
+        [utilities addVariableTo:mesh.variables mesh:mesh solution:solution name:@"coordinate 2" dofs:1 container:varContainers component:NO ifOutput:NULL ifSecondary:NULL type:NULL];
+        
+        varContainers->Values = nodes->z;
+        [utilities addVariableTo:mesh.variables mesh:mesh solution:solution name:@"coordinate 3" dofs:1 container:varContainers component:NO ifOutput:NULL ifSecondary:NULL type:NULL];
+        
+        varContainers->Values = _sTime;
+        varContainers->sizeValues = _sizeSTime;
+        [utilities addVariableTo:mesh.variables mesh:mesh solution:solution name:@"time" dofs:1 container:varContainers component:NO ifOutput:NULL ifSecondary:NULL type:NULL];
+        
+        varContainers->Values = _sStep;
+        varContainers->sizeValues = _sizeSStep;
+        [utilities addVariableTo:mesh.variables mesh:mesh solution:solution name:@"time step" dofs:1 container:varContainers component:NO ifOutput:NULL ifSecondary:NULL type:NULL];
+        
+        varContainers->Values = _sSize;
+        varContainers->sizeValues = _sizeSSize;
+        [utilities addVariableTo:mesh.variables mesh:mesh solution:solution name:@"time step size" dofs:1 container:varContainers component:NO ifOutput:NULL ifSecondary:NULL type:NULL];
+        
+        varContainers->Values = _sInterval;
+        varContainers->sizeValues = _sizeSInterval;
+        [utilities addVariableTo:mesh.variables mesh:mesh solution:solution name:@"time step interval" dofs:1 container:varContainers component:NO ifOutput:NULL ifSecondary:NULL type:NULL];
+        
+        // Save some previous time steps for variable time step multistep methods
+        dtVar = [utilities getVariableFrom:mesh.variables model:model name:@"time step size" onlySearch:NULL maskName:NULL info:&found];
+        dtVarContainers = dtVar.getContainers;
+        dtVarContainers->PrevValues = _sPrevSizes;
+        dtVarContainers->size1PrevValues = _size1SPrevSizes;
+        dtVarContainers->size2PrevValues = _size22SPrevSizes;
+        
+        varContainers->Values = _nonLinIt;
+        varContainers->sizeValues = _sizeNonLinIt;
+        [utilities addVariableTo:mesh.variables mesh:mesh solution:solution name:@"nonlin iter" dofs:1 container:varContainers component:NO ifOutput:NULL ifSecondary:NULL type:NULL];
+        
+        varContainers->Values = _steadyIt;
+        varContainers->sizeValues = _sizeSteadyIt;
+        [utilities addVariableTo:mesh.variables mesh:mesh solution:solution name:@"coupled iter" dofs:1 container:varContainers component:NO ifOutput:NULL ifSecondary:NULL type:NULL];
+    }
+    free(varContainers);
 }
 
 @end
